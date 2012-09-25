0bcbb4cb2d11fb3936c0fc61daa031f9b5b9c4e6
Ben Slavin <ben@bolsterlabs.com>
2012-09-25 08:48:05 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import re
+import os
+import sys
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.match("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
+version = get_version('uploadifive')
+
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    print "You probably want to also tag the version now:"
+    print "  git tag -a %s -m 'version %s'" % (version, version)
+    print "  git push --tags"
+    sys.exit()
+
+
+setup(
+    name='uploadifive',
+    version=version,
+    url='http://github.com.org/bolster/django-uploadifive',
+    license='BSD',
+    description='Django Out-of-Band Form Uploads',
+    author='Bolster Labs, Inc.',
+    author_email='info@bolsterlabs.com',
+    packages=get_packages('uploadifive'),
+    package_data=get_package_data('uploadifive'),
+    install_requires=[
+        'Django>=1.4.1',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
